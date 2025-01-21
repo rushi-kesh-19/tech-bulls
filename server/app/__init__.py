0f6464e7203a37b9d629520af077a442a7f71873
Rsoniie <roshansoniee@gmail.com>
2025-01-21 11:25:19 +0530

    Backend Started

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,11 +1,11 @@
+
 from flask import Flask
 
 def create_app():
+
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 7870
 
     from app.routes import main
     app.register_blueprint(main)
 
     return app
- 
